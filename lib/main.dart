2ca7a07ec793d9f217679f0f57f885937eb7eb13
mahesh <mahehscn31@gmail.com>
2020-09-23 19:41:47 +0530

    container and text widgets added to body

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,33 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("First App"),
         ),
-        body: Container(
-          child: Center(child: Text("Hi Flutter")),
+        body: Center(
+          child: Container(
+            width: 250,
+            height: 200,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              // shape: BoxShape.circle,
+              color: Colors.teal,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey[500],
+                    blurRadius: 5,
+                    offset: Offset(2.0, 5.0))
+              ],
+              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink]),
+            ),
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              "Text inside container",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
         ));
   }
 }
