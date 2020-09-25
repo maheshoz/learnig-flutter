a6983cbb9529727546630908408df8aa768b0f5e
mahesh <mahehscn31@gmail.com>
2020-09-25 19:18:23 +0530

    created dart widget files from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:first_flutter_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
+
+
 
 void main() {
   runApp(MaterialApp(
@@ -10,96 +13,3 @@ void main() {
   ));
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  TextEditingController _nameController = TextEditingController();
-  var myText = "Change me";
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text("First App"),
-      ),
-      drawer: Drawer(
-          child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          UserAccountsDrawerHeader(
-            accountName: Text("John"),
-            accountEmail: Text("example@gmail.com"),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://randomuser.me/api/portraits/men/97.jpg"),
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text("Account"),
-            subtitle: Text("personal"),
-            trailing: Icon(Icons.edit),
-          ),
-          ListTile(
-            leading: Icon(Icons.email),
-            title: Text("Email"),
-            subtitle: Text("email.gmail.com"),
-            trailing: Icon(Icons.send),
-          )
-        ],
-      )),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child: Column(
-              children: [
-                Image.asset(
-                  "assets/bg2.jpg",
-                  fit: BoxFit.cover,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  myText,
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: TextField(
-                      controller: _nameController,
-                      keyboardType: TextInputType.text,
-                      // obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: "Enter name",
-                        labelText: "Name",
-                      ),
-                    )),
-              ],
-            ),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          myText = _nameController.text;
-          setState((){});
-        },
-        child: Icon(Icons.refresh),
-      ),
-    );
-  }
-}
