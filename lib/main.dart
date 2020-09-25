29b41a856c6ff3dddef4779c9b5d32aac2670b19
mahesh <mahehscn31@gmail.com>
2020-09-25 15:43:27 +0530

    learned about Row, Column, Align widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,36 +11,46 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("First App"),
-        ),
-        body: Center(
+      appBar: AppBar(
+        title: Text("First App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          alignment: Alignment.centerLeft,
           child: Container(
-            width: 250,
-            height: 200,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              // shape: BoxShape.circle,
-              color: Colors.teal,
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey[500],
-                    blurRadius: 5,
-                    offset: Offset(2.0, 5.0))
-              ],
-              gradient: LinearGradient(colors: [Colors.yellow, Colors.pink]),
-            ),
-            padding: const EdgeInsets.all(8),
-            child: Text(
-              "Text inside container",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+            color: Colors.black,
+            // width: MediaQuery.of(context).size.width,
+            // height: MediaQuery.of(context).size.height,
+            width: 200,
+            height: 400,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                ],
               ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
